77375a0e56c81a048c4b78664e1b29a61b1c9117
carlos <cvelasco@skourse.com>
2022-05-21 21:37:20 +0800

    add login form

---
+++ src/components/Signup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Signup = () => {
+  return <Container>Signup</Container>;
+};
+
+export default Signup;
+
+const Container = styled.div``;
